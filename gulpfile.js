0d9b769578ec19d0173a711d28e7c001e84a6a0a
Nitin Surana <coding_idiot@Nitins-MacBook-Pro.local>
2016-02-10 02:30:45 +0530

    Added wildcard to combine all *Recipe.js into inject.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,8 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 
 gulp.task('scripts', function () {
-    gulp.src(['common.js', 'PassageRecipe.js'])
+    gulp.src(['common.js', '*Recipe.js'])
         .pipe(concat('inject.js'))
         .pipe(uglify())
-        //.pipe(gulp.dest('inject_uglified.js'))
         .pipe(gulp.dest('./'));
 });
