b86eb51759524d7b2d3401f1057279d55d90b0f2
Himadri <himadri@Snapwizs-MacBook-Pro-2.local>
2016-02-19 00:09:53 +0530

    Glider : First Recipe - Login

---
+++ src/recipes/LoginGliderRecipe.js
@@ -0,0 +1,21 @@
+(function () {
+    var steps = [
+        {
+            selector: '#at-field-email',
+            action: 'input'
+        },
+        {
+            selector: '#at-field-password',
+            action: 'input'
+        },
+        {
+            selector: '.btn-steplr-submit'
+        }
+    ];
+    var recipe = window.recipe.LoginGliderRecipe = new window.recipe.Recipe(steps);
+    recipe.start = function (params) {
+        this.steps[0].value = params.self.email;
+        this.steps[1].value = params.self.password;
+        return $.Deferred().resolve();
+    }
+})();
