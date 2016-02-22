cec7fe51c238b7e42c6391f7f2ac3d2b5f788cb7
chetan <chetan.narian@gmail.com>
2016-02-22 16:25:58 +0530

    Added Fill Assignment Details and Adding Classic Question Recipe

---
+++ src/recipes/FillClassicAssignmenttoCreateAssignmentLSRecipe.js
@@ -0,0 +1,29 @@
+(function () {
+    'use strict';
+    
+    var steps = [
+				{
+					selector: '#ls-ins-assignment-name',
+					action: 'click',
+				},
+        {
+            selector: '#ls-ins-edit-assignment',
+            action: 'input',
+						value: '{datetime} - Assignment Title'
+        },
+				{
+					selector: '#ls-ins-assignment-desc',
+					action: 'click',
+				},
+        {
+            selector: '#ls-ins-enter-assignment-desc',
+            action: 'input',
+						value: '{datetime} - Assignment Description'
+        },
+				{
+					selector: 'body',
+					action: 'click'
+				}
+    ];
+    var recipe = window.recipe.FillClassicAssignmenttoCreateAssignmentLSRecipe = new window.recipe.Recipe(steps);
+})();
