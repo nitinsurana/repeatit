fb1c97c3cf90bb18b22c593ee092a805fb77b990
Nitin Surana <nitin.cool4urchat@gmail.com>
2016-02-27 02:44:42 +0530

    Added analytics heroku nodejs-mongo app with socket.io to show usages in real-time. Todo : 1. Connect to mongolab and deploy on heroku 2. Add ability for extension to report usages to heroku

---
+++ analytics/index.js
@@ -0,0 +1,57 @@
+/* jshint ignore:start */
+
+var express = require('express'),
+    exphbs = require('express-handlebars'),
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser'),
+    app = express()
+        .use(bodyParser.json()) // support json encoded bodies
+        .use(bodyParser.urlencoded({extended: true})) // support encoded bodies
+
+        .set('views', __dirname + '/views')
+        .set('view engine', 'handlebars')
+        .engine('handlebars', exphbs({defaultLayout: 'main'})),
+    http = require('http').Server(app),
+    io = require('socket.io')(http),
+
+// Mongoose Schema definition
+    Schema = new mongoose.Schema({
+        at: Number,
+        recipeName: String
+    }),
+    Usage = mongoose.model('Usage', Schema);
+
+/*
+ * MONGOLAB_URI=mongodb://example:example@ds053312.mongolab.com:53312/todolist
+ * 'mongodb://example:example@ds053312.mongolab.com:53312/todolist'
+ */
+mongoose.connect('mongodb://localhost:27017/repeatit', function (error) {
+    if (error) console.error(error);
+    else console.log('mongo connected');
+});
+
+
+app.get('/', function (req, res) {
+    res.render('home');
+})
+    .get('/usages', function (req, res) {
+        Usage.find(function (err, usages) {     // http://mongoosejs.com/docs/api.html#query_Query-find
+            res.status(200).json(usages)
+        });
+    })
+    .post('/usage', function (req, res) {
+        var usage = new Usage(req.body);
+        usage.id = usage._id;
+        usage.save(function (err) {     // http://mongoosejs.com/docs/api.html#model_Model-save
+            res.status(200).json(usage);
+            io.emit('new usage',usage.toJSON());
+        });
+    })
+
+    .use(express.static(__dirname + '/'));
+
+http.listen(process.env.PORT || 5000);
+
+io.on('connection', function (socket) {
+    console.log('a user connected');
+});
---
+++ analytics/views/main.js
@@ -0,0 +1,37 @@
+(function () {
+    'use strict';
+
+    var UsageCollection = Backbone.Collection.extend({
+        url: '/usages'
+    });
+
+    var DashboardView = Backbone.View.extend({
+        el: 'body',
+        initialize: function () {
+            var self = this;
+            this.collection = new UsageCollection();
+            this.usageTemplate = $("#usageTemplate").html();
+            this.collection.fetch().done(function () {
+                self.render();
+            });
+            socket.on('new usage', function (model) {
+                self.collection.add(model);
+            });
+            this.collection.on('add', this.addUsage, this);
+        },
+        render: function () {
+            var self = this;
+            _.each(this.collection.models, function (model) {
+                self.addUsage(model);
+            });
+        },
+        addUsage: function (model) {
+            var html = _.template(this.usageTemplate)({data: model.toJSON()});
+            this.$("#usageList tbody").prepend($(html).hide().slideDown());
+        }
+    });
+
+    var v = new DashboardView();
+})();
+
+
