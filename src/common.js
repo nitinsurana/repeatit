99805c608a49def177c1e0ca8c4b42868b66b9c9
motikant-sw <motikant.polai@snapwiz.com>
2016-03-05 00:28:50 +0530

    Added browser detection function to recipe.utils and made it publicly available

--- src/common.js
+++ src/common.js
@@ -188,8 +188,38 @@
             str = str.replace(/{timeStamp\(?[\d]*\)?}/g, function (match) {     //jshint ignore:line
                 return self.timeStampReplacer.call(self, match);
             }); //jshint ignore:line
+			str = str.replace(/{browser}/g,function(){
+				return self.detectBrowser();
+			});
             return str;
         },
+		detectBrowser: function(){
+			// See http://stackoverflow.com/a/9851769
+			if(this.browser){
+				return this.browser;
+			}
+			var isOpera = (!!window.opr && !!window.opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+			// Firefox 1.0+
+			var isFirefox = typeof InstallTrigger !== 'undefined';
+			// At least Safari 3+: "[object HTMLElementConstructor]"
+			var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
+			// Internet Explorer 6-11
+            var isIE = /*@cc_on!@*/false || !!document.documentMode;
+			// Edge 20+
+			var isEdge = !isIE && !!window.StyleMedia;
+			// Chrome 1+
+			var isChrome = !!window.chrome && !!window.chrome.webstore;
+			// Blink engine detection
+			var isBlink = (isChrome || isOpera) && !!window.CSS;
+
+			return (this.bowser =
+			        isOpera ? 'Opera' :
+			        isFirefox ? 'Firefox' :
+			        isSafari ? 'Safari' :
+			        isChrome ? 'Chrome' :
+			        isIE ? 'IE' :
+			        'other');
+		},
         randomStringReplacer: function (match) {
             var length = match.replace(/[a-zA-Z\(\)\{\}]/g, "");
             length = window.isNaN(window.parseInt(length, 10)) ? 5 : window.parseInt(length, 10);
