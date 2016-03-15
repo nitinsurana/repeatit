nitinsurana/repeatit

commit 85f4bb28a2a3478ab0e345abb3ace6d1150fca2c

1 . Glider Flow : Employer sign up with new company.  2. UI Change : Bootstrap badge instead of button

src/popup.js
@@ -15,14 +15,19 @@ $(function () {
                 continue;
             }
             var $li = $("<li>", {
-                "data-recipeid": recipelist[i].id
-            }).html("<span>" + recipelist[i].title + "</span>");
+                "data-recipeid": recipelist[i].id,
+                "class" : "row"
+            }).html("<div class='col-6 controlled-text'>" + recipelist[i].title + "</div>");
+            var $params = "";
             if (recipelist[i].parameterSets && Object.keys(recipelist[i].parameterSets).length > 0) {
                 for (var setName in recipelist[i].parameterSets) {
-                    //Todo use <span class="badge">4</span> instead of button
-                    $li.append('<button type="button" class="pull-right btn btn-default btn-xs" data-setname="' + setName + '">' + setName + '</button>');
+                    $params += "<span class='pull-right label label-primary ri-param-badge' data-setname='" + setName + "'>" + setName + "</span>";
                 }
             }
+            var $div = $("<div>", {
+                "class" : "col-6"
+            }).html($params);
+            $li.append($div);
             $results.append($li);
         }
     }

src/child_recipes/SignUpFlowFillSignUpPageRecipe.js
@@ -1,0 +1,37 @@
+(function(){
+  'use strict';
+
+  var steps = [
+    {
+      selector: '#at-field-fullname',
+      action: "input"
+    },
+    {
+      selector: '#at-field-mobile',
+      action: "input"
+    },
+    {
+      selector: '#at-field-email',
+      action: "input"
+    },
+    {
+      selector: '#at-field-password',
+      action: "input"
+    },
+    {
+      selector: '#at-field-terms'
+    },
+    {
+      selector: '#signup-button'
+    }
+  ];
+
+  var recipe = window.recipe.SignUpFlowFillSignUpPageRecipe = new window.recipe.Recipe(steps);
+  recipe.start = function (params) {
+    this.steps[0].value = params.name;
+    this.steps[1].value = params.phone;
+    this.steps[2].value = params.email;
+    this.steps[3].value = params.password;
+    return $.Deferred().resolve();
+  };
+})();

src/child_recipes/SignUpFlowYourRoleRecipe.js
@@ -1,0 +1,14 @@
+(function(){
+  'use strict';
+
+  var steps = [
+    {
+      selector: ".selectable-box-label:eq(0)"
+    }
+  ];
+
+  var recipe = window.recipe.SignUpFlowYourRoleRecipe = new window.recipe.Recipe(steps);
+  recipe.start = function (params) {
+    return $.Deferred().resolve();
+  };
+})();

src/css/custom.css
@@ -11,7 +11,29 @@ body, section {
     overflow-x: hidden;
 }
 
+.ri-param-badge {
+    margin: 2px 5px 2px 0px;
+    cursor: pointer;
+}
+
+.controlled-text {
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
+}
+
 /* Popup.html Page Styles */
+.ri-search {
+    border-radius: 0px;
+    box-shadow: none;
+    margin-bottom: 5px;
+}
+
+.ri-search:focus {
+    border-color: #8180d4;
+    box-shadow: none;
+}
+
 .popup-body {
     padding: 0px 3px;
 }
@@ -26,6 +48,10 @@ body, section {
 
 .popup-body .ri-tabs-container .ri-tabs li a {
     padding: 4px 10px;
+}
+
+.popup-body .ri-tabs-container .result-body {
+    padding: 0px;
 }
 
 /* Basic style tweaks */
@@ -51,19 +77,28 @@ section {
 }
 
 #results li {
-    cursor: pointer;
-    padding: 5px;
-    border: 1px solid;
-    background: lightgoldenrodyellow;
+    //cursor: pointer;
+    padding: 10px;
+    border-bottom: 1px solid #ccc;
+    //border: 1px solid;
+    //background: lightgoldenrodyellow;
 }
 
-#results li:hover, #results li:nth-child(2):hover {
-    background: orange;
+#results li:last-child {
+    border-bottom: none;
 }
 
-#results li:nth-child(2n) {
-    background: greenyellow;
+#results li:hover {
+    background-color: #e9e9e9;
 }
+
+/*#results li:hover, #results li:nth-child(2):hover {*/
+    /*background: orange;*/
+/*}*/
+
+/*#results li:nth-child(2n) {*/
+    /*background: greenyellow;*/
+/*}*/
 
 /* Options Html - Start */
 .parameter-sets label{

src/popup.html
@@ -30,6 +30,7 @@
                 <button id="closePopup" class="btn btn-outline btn-danger ri-record-btn" data-toggle="tooltip" data-placement="bottom" title="Sign off"><i class="glyphicon glyphicon-log-out"></i></button>
             </span>
         </div>
+        <input type='search' class="form-control ri-search" id="search" name="search" autocomplete="off" placeholder="Find Recipe...">
         <div class="tabs-container ri-tabs-container">
             <ul class="nav nav-tabs ri-tabs">
                 <li class="tab-ls active"><a data-toggle="tab" data-project="ls" href="#result-panel"><img src="img/ls-logo.png" style="height: 10px"></a></li>
@@ -39,7 +40,6 @@
             <div class="tab-content ri-content">
                 <div id="result-panel" class="tab-pane active">
                     <div class="panel-body result-body">
-                        <input type='search' class="form-control" id="search" name="search" autocomplete="off" placeholder="Find Recipe...">
                         <ul class="list-unstyled" id="results">
                         </ul>
                     </div>

src/recipes/GliderEmployerSignUpRecipe.js
@@ -1,0 +1,26 @@
+(function(){
+  'use strict';
+
+  var steps = [
+    {
+      type: 'recipe',
+      recipeId: 'SignUpFlowFillCompanyDetailsRecipe'
+    },
+    {
+      type: 'recipe',
+      recipeId: "SignUpFlowYourRoleRecipe"
+    },
+    {
+      type: 'recipe',
+      recipeId: "SignUpFlowFillSignUpPageRecipe",
+      params: {}
+    }
+  ];
+
+  var recipe = window.recipe.GliderEmployerSignUpRecipe = new window.recipe.Recipe(steps);
+  recipe.start = function (params) {
+    console.log(params);
+    this.steps[2].params = params.self;
+    return $.Deferred().resolve();
+  };
+})();

commit 1c88c9dcdd081388b9e4d4536e59d49091b24691

Fixed Bug : Clicking on recipe with paramSet is throwing error in console because it's trying to execute the recipe without paramSet. Happens when user clicks on <li> instead of appropriate paramSet defining <span.badge>s Root Cause : Broken in 85f4bb28a2a3478ab0e345abb3ace6d1150fca2c, due to change to button to span with badge

src/popup.js
@@ -112,7 +112,7 @@ $(function () {
     $results.on('click', 'li', function (e) {
         var recipeid = $(e.currentTarget).data('recipeid');
         var setName = $(e.target).data('setname');
-        if ($(e.currentTarget).find("button.btn-xs").length > 0 && !setName) {      //Don't run recipe with require paramSets without a paramSet (click on btn instead of li)
+        if ($(e.currentTarget).find("span.ri-param-badge").length > 0 && !setName) {      //Don't run recipe, which require paramSets, without a paramSet (click on li instead of span)
             return;
         }
         runRecipe(recipeid, setName);